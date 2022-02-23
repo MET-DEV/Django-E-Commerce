236d838d4255c895e8797002db1d7a86e84f36ce
MET-DEV <mustafatpe23@gmail.com>
2022-02-23 20:15:35 +0300

    filter by category's path changed

--- babyshop_app/products/urls.py
+++ babyshop_app/products/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns=[
     path('',views.product_list,name='products'),
-    path('categories<slug:category_slug>',views.product_list,name="products_by_category"),
+    path('categories/<slug:category_slug>',views.product_list,name="products_by_category"),
     path('<slug:category_slug>/<int:product_id>',views.product_detail,name="product_detail")
 
 ]
